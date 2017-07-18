2d76f68ba83088d67c5954102214ed16611a09d2
Matvey Arye <mat@timescale.com>
2017-07-18 13:19:22 -0400

    pgindent

--- src/parse.c
+++ src/parse.c
@@ -8,197 +8,201 @@
 
 #define MAX_NAMELEN 1024
 
-#define fail(format, ...)                                               \
-    ereport(ERROR,                                                      \
-            (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),              \
-             errmsg("invalid input syntax for prometheus sample: " format, \
-                    ##__VA_ARGS__)));
+#define fail(format, ...)												\
+	ereport(ERROR,														\
+			(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),				\
+			 errmsg("invalid input syntax for prometheus sample: " format, \
+					##__VA_ARGS__)));
 
 typedef struct PrometheusParseCtx
 {
-    PrometheusSample *sample;
-    uint16 numlabels;
-    uint32 numchars;
+	PrometheusSample *sample;
+	uint16		numlabels;
+	uint32		numchars;
 } PrometheusParseCtx;
 
 static int
 parse_labels(char *input, PrometheusParseCtx *ctx)
 {
-    PrometheusLabel *curr = ctx->sample == NULL ? NULL : PROM_LABELS(ctx->sample);
-    int i = 0;
+	PrometheusLabel *curr = ctx->sample == NULL ? NULL : PROM_LABELS(ctx->sample);
+	int			i = 0;
 
-    if (input[i] != '{')
-    {
-        return 0;
-    }
+	if (input[i] != '{')
+	{
+		return 0;
+	}
 
-    ctx->numlabels = 0;
-    ctx->numchars = 0;
+	ctx->numlabels = 0;
+	ctx->numchars = 0;
 
-    /* Skip opening brace */
-    i++;
+	/* Skip opening brace */
+	i++;
 
-    while (input[i] != '}' && input[i] != '\0')
-    {
-        int count;
-        char labelname[MAX_NAMELEN];
-        int value_idx;
-        char *value_start;
-        size_t namelen;
-        size_t valuelen;
+	while (input[i] != '}' && input[i] != '\0')
+	{
+		int			count;
+		char		labelname[MAX_NAMELEN];
+		int			value_idx;
+		char	   *value_start;
+		size_t		namelen;
+		size_t		valuelen;
 
-        count = sscanf(input + i, "%c%1023[a-zA-Z0-9_]=%n",
-                       &labelname[0], &labelname[1], &value_idx);
+		count = sscanf(input + i, "%c%1023[a-zA-Z0-9_]=%n",
+					   &labelname[0], &labelname[1], &value_idx);
 
-        if (count != 2)
-        {
-            fail("Unexpected number of input items assigned: %d\n", count);
-        }
+		if (count != 2)
+		{
+			fail("Unexpected number of input items assigned: %d\n", count);
+		}
 
-        if (!((labelname[0] >= 'a' &&
-               labelname[0] <= 'z') ||
-              (labelname[0] >= 'A' &&
-               labelname[0] <= 'Z') ||
-              labelname[0] == '_'))
-        {
-            /* Invalid first char */
-            fail("Invalid first label name char %c\n", labelname[0]);
-        }
+		if (!((labelname[0] >= 'a' &&
+			   labelname[0] <= 'z') ||
+			  (labelname[0] >= 'A' &&
+			   labelname[0] <= 'Z') ||
+			  labelname[0] == '_'))
+		{
+			/* Invalid first char */
+			fail("Invalid first label name char %c\n", labelname[0]);
+		}
 
-        namelen = value_idx - 1;
-        i += value_idx;
+		namelen = value_idx - 1;
+		i += value_idx;
 
-        if (input[i] != '"')
-        {
-            fail("Label not enclosed by double quotes (start)\n");
-        }
+		if (input[i] != '"')
+		{
+			fail("Label not enclosed by double quotes (start)\n");
+		}
 
-        i++;
+		i++;
 
-        value_start = input + i;
+		value_start = input + i;
 
-        while (input[i] != '"' && input[i] != '}')
-        {
-            i++;
-        }
+		while (input[i] != '"' && input[i] != '}')
+		{
+			i++;
+		}
 
-        if (input[i] != '"')
-        {
-            fail("Label not enclosed by double quotes (end): %c\n", input[i]);
-        }
+		if (input[i] != '"')
+		{
+			fail("Label not enclosed by double quotes (end): %c\n", input[i]);
+		}
 
-        valuelen = input + i - value_start;
+		valuelen = input + i - value_start;
 
-        if (curr != NULL)
-        {
-            PROM_LABEL_NAME_SET(curr, labelname, namelen);
-            PROM_LABEL_VALUE_SET(curr, value_start, valuelen);
-            curr = PROM_LABEL_NEXT(curr);
-        }
+		if (curr != NULL)
+		{
+			PROM_LABEL_NAME_SET(curr, labelname, namelen);
+			PROM_LABEL_VALUE_SET(curr, value_start, valuelen);
+			curr = PROM_LABEL_NEXT(curr);
+		}
 
-        i++;
-        ctx->numlabels++;
-        ctx->numchars += namelen + valuelen + 2;
+		i++;
+		ctx->numlabels++;
+		ctx->numchars += namelen + valuelen + 2;
 
-        if (input[i] == ',')
-        {
-            i++;
-        } else {
-            break;
-        }
-    }
+		if (input[i] == ',')
+		{
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
 
-    if (input[i] != '}')
-    {
-        /* Unexpected state */
-        fail("Unexpected end char %c\n", input[i]);
-    }
+	if (input[i] != '}')
+	{
+		/* Unexpected state */
+		fail("Unexpected end char %c\n", input[i]);
+	}
 
-    return i + 1;
+	return i + 1;
 }
 
 PrometheusSample *
 prom_from_cstring(char *input)
 {
-    int ret;
-    char       metric_name[MAX_NAMELEN];
-    double     value;
-    int64_t    time_ms;
-    int        label_start_idx;
-    //size_t label_count;
-    size_t metric_namelen;
-    PrometheusParseCtx ctx = { 0 };
-    PrometheusSample *sample;
-    size_t samplelen;
+	int			ret;
+	char		metric_name[MAX_NAMELEN];
+	double		value;
+	int64_t		time_ms;
+	int			label_start_idx;
 
-    ret = sscanf(input, "%c%1023[a-zA-Z0-9_:]%n",
-                 &metric_name[0], &metric_name[1], &label_start_idx);
+	/* size_t label_count; */
+	size_t		metric_namelen;
+	PrometheusParseCtx ctx = {0};
+	PrometheusSample *sample;
+	size_t		samplelen;
 
-    if (ret != 2)
-    {
-        fail("Unexpected number of input items assigned: %d\n", ret);
-    }
+	ret = sscanf(input, "%c%1023[a-zA-Z0-9_:]%n",
+				 &metric_name[0], &metric_name[1], &label_start_idx);
 
-    /* Check first char */
-    if (!((metric_name[0] >= 'a' &&
-           metric_name[0] <= 'z') ||
-          (metric_name[0] >= 'A' &&
-           metric_name[0] <= 'Z') ||
-          metric_name[0] == '_'  ||
-          metric_name[0] == ':'))
-    {
-        /* Invalid first char */
-        fail("Invalid first char %c\n", metric_name[0]);
-    }
+	if (ret != 2)
+	{
+		fail("Unexpected number of input items assigned: %d\n", ret);
+	}
 
-    input += label_start_idx;
-    metric_namelen = label_start_idx;
+	/* Check first char */
+	if (!((metric_name[0] >= 'a' &&
+		   metric_name[0] <= 'z') ||
+		  (metric_name[0] >= 'A' &&
+		   metric_name[0] <= 'Z') ||
+		  metric_name[0] == '_' ||
+		  metric_name[0] == ':'))
+	{
+		/* Invalid first char */
+		fail("Invalid first char %c\n", metric_name[0]);
+	}
 
-    /* Calculate labels length */
-    ret = parse_labels(input, &ctx);
+	input += label_start_idx;
+	metric_namelen = label_start_idx;
 
-    if (ret < 0)
-    {
-        fail("Bad label set\n");
-    }
-    samplelen = PROM_ALLOC_LEN(metric_namelen, ctx.numlabels, ctx.numchars);
+	/* Calculate labels length */
+	ret = parse_labels(input, &ctx);
 
-    sample = palloc(samplelen);
-    memset(sample, 0, samplelen);
-    sample->numlabels = ctx.numlabels;
-    SET_VARSIZE(sample, samplelen);
-    PROM_NAME_SET(sample, metric_name, metric_namelen);
-    ctx.sample = sample;
+	if (ret < 0)
+	{
+		fail("Bad label set\n");
+	}
+	samplelen = PROM_ALLOC_LEN(metric_namelen, ctx.numlabels, ctx.numchars);
 
-    if (ctx.numlabels > 0)
-    {
-        /* Parse for real */
-        ret = parse_labels(input, &ctx);
+	sample = palloc(samplelen);
+	memset(sample, 0, samplelen);
+	sample->numlabels = ctx.numlabels;
+	SET_VARSIZE(sample, samplelen);
+	PROM_NAME_SET(sample, metric_name, metric_namelen);
+	ctx.sample = sample;
 
-        if (ret < 0)
-        {
-            fail("Bad label set\n");
-        }
-    }
+	if (ctx.numlabels > 0)
+	{
+		/* Parse for real */
+		ret = parse_labels(input, &ctx);
 
-    input += ret;
+		if (ret < 0)
+		{
+			fail("Bad label set\n");
+		}
+	}
 
-    ret = sscanf(input, " %lf %" PRId64 "", &value, &time_ms);
+	input += ret;
 
-    if (ret < 1)
-    {
-        fail("Unexpected number of input items assigned\n");
-    }
+	ret = sscanf(input, " %lf %" PRId64 "", &value, &time_ms);
 
-    if (ret == 1)
-    {
-        struct timeval now;
-        gettimeofday(&now, NULL);
-        time_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
-    }
+	if (ret < 1)
+	{
+		fail("Unexpected number of input items assigned\n");
+	}
 
-    sample->value = value;
-    sample->time = prom_unix_microseconds_to_timestamp(time_ms * 1000);
+	if (ret == 1)
+	{
+		struct timeval now;
 
-    return sample;
+		gettimeofday(&now, NULL);
+		time_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
+	}
+
+	sample->value = value;
+	sample->time = prom_unix_microseconds_to_timestamp(time_ms * 1000);
+
+	return sample;
 }
--- src/utils.c
+++ src/utils.c
@@ -9,30 +9,30 @@
 int64
 prom_timestamp_to_unix_microseconds(TimestampTz timestamp)
 {
-    int64       epoch_diff_microseconds = (POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
-    int64       microseconds;
+	int64		epoch_diff_microseconds = (POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY;
+	int64		microseconds;
 
-    if (timestamp < MIN_TIMESTAMP)
-        ereport(ERROR,
-                (errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
-                 errmsg("timestamp out of range")));
+	if (timestamp < MIN_TIMESTAMP)
+		ereport(ERROR,
+				(errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
+				 errmsg("timestamp out of range")));
 
-    if (timestamp >= (END_TIMESTAMP - epoch_diff_microseconds))
-        ereport(ERROR,
-                (errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
-                 errmsg("timestamp out of range")));
+	if (timestamp >= (END_TIMESTAMP - epoch_diff_microseconds))
+		ereport(ERROR,
+				(errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
+				 errmsg("timestamp out of range")));
 
 #ifdef HAVE_INT64_TIMESTAMP
-    microseconds = timestamp + epoch_diff_microseconds;
+	microseconds = timestamp + epoch_diff_microseconds;
 #else
-    if (1)
-    {
-        int64       seconds = (int64) timestamp;
+	if (1)
+	{
+		int64		seconds = (int64) timestamp;
 
-        microseconds = (seconds * USECS_PER_SEC) + ((timestamp - seconds) * USECS_PER_SEC) + epoch_diff_microseconds;
-    }
+		microseconds = (seconds * USECS_PER_SEC) + ((timestamp - seconds) * USECS_PER_SEC) + epoch_diff_microseconds;
+	}
 #endif
-    return microseconds;
+	return microseconds;
 }
 
 /*
@@ -41,27 +41,27 @@ prom_timestamp_to_unix_microseconds(TimestampTz timestamp)
 TimestampTz
 prom_unix_microseconds_to_timestamp(int64 microseconds)
 {
-    TimestampTz timestamp;
+	TimestampTz timestamp;
 
-    /*
-     * Test that the UNIX us timestamp is within bounds. Note that an int64 at
-     * UNIX epoch and microsecond precision cannot represent the upper limit
-     * of the supported date range (Julian end date), so INT64_MAX is the
-     * natural upper bound for this function.
-     */
-    if (microseconds < ((int64) USECS_PER_DAY * (DATETIME_MIN_JULIAN - UNIX_EPOCH_JDATE)))
-        ereport(ERROR,
-                (errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
-                 errmsg("timestamp out of range")));
+	/*
+	 * Test that the UNIX us timestamp is within bounds. Note that an int64 at
+	 * UNIX epoch and microsecond precision cannot represent the upper limit
+	 * of the supported date range (Julian end date), so INT64_MAX is the
+	 * natural upper bound for this function.
+	 */
+	if (microseconds < ((int64) USECS_PER_DAY * (DATETIME_MIN_JULIAN - UNIX_EPOCH_JDATE)))
+		ereport(ERROR,
+				(errcode(ERRCODE_DATETIME_VALUE_OUT_OF_RANGE),
+				 errmsg("timestamp out of range")));
 
 #ifdef HAVE_INT64_TIMESTAMP
-    timestamp = microseconds - ((POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY);
+	timestamp = microseconds - ((POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY);
 #else
-    /* Shift the epoch using integer arithmetic to reduce precision errors */
-    timestamp = microseconds / USECS_PER_SEC;   /* seconds */
-    microseconds = microseconds - ((int64) timestamp * USECS_PER_SEC);
-    timestamp = (float8) ((int64) seconds - ((POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * SECS_PER_DAY))
-        + (float8) microseconds / USECS_PER_SEC;
+	/* Shift the epoch using integer arithmetic to reduce precision errors */
+	timestamp = microseconds / USECS_PER_SEC;	/* seconds */
+	microseconds = microseconds - ((int64) timestamp * USECS_PER_SEC);
+	timestamp = (float8) ((int64) seconds - ((POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * SECS_PER_DAY))
+		+ (float8) microseconds / USECS_PER_SEC;
 #endif
-    return timestamp;
+	return timestamp;
 }
